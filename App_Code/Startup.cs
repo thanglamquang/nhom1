954762ad098f3a7ffcc4439dd66a52792578e847
thanglam <lamthang771998@gmail.com>
2018-10-08 17:06:59 +0700

    first commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nhom1_quanlybanhang.Startup))]
+namespace nhom1_quanlybanhang
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
